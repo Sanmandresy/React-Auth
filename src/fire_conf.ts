e2eb7dea333034a458a8f37f265ade7b6de4cd7d
Sanmandresy <hei.sanda.3@gmail.com>
2022-08-14 18:56:29 +0300

    fix: .env

--- src/fire_conf.ts
+++ src/fire_conf.ts
@@ -5,13 +5,14 @@ import {getFirestore, query, getDocs, collection, where, addDoc} from "firebase/
 
 
 const firebaseConfig = {
-    apiKey:"AIzaSyB0Dg2C1sJv271k736sU3-OFrX0ZGCiX1k",
-    authDomain: "react-ts-firebase-b6b11.firebaseapp.com",
-    projectId: "react-ts-firebase-b6b11",
-    storageBucket: "react-ts-firebase-b6b11.appspot.com",
-    messagingSenderId: "823967191861",
-    appId: "1:823967191861:web:293f6001f37ae6360d727c",
-    measurementId: "G-2G1E23YHSZ"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 
